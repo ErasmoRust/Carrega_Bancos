99ecbe1cfc09a9cecd10ba58b9be6e9e5f40ad4a
ErasmoRust <erasmo.rust42@gmail.com>
2024-12-02 22:27:25 -0300

    Segunda_Versao

--- Carrega_bancos_dados.py
+++ Carrega_bancos_dados.py
@@ -7,18 +7,22 @@ Created on Mon Dec  2 21:22:07 2024
 
 #%%
 import pandas as pd
-import numpy as np
-
-#%%
-#importando os bancos de dados em CSV
-
-dados_cvm = pd.read_csv('nomecsv.csv',
-                        sep=';', #tipo de separador
-                        encoding = 'latin1')#caso contenha caracteres especiais
-
-
 
 #%% 
 #importando os bancos de dados em XLSX
-nome_objeto1 = pd.read_excel('nometabela.xls')
-nome_objeto2 = pd.read_excel('nometabela')
+
+nome_objeto = pd.read_excel('nome_tabela.xls')
+
+#%%
+
+#importando os bancos de dados em CSV
+
+nome_objeto = pd.read_csv('nome_arquivo.csv', 
+                          sep=',')   #Tipo de separador
+                                     
+
+nome_objeto = pd.read_csv('nome_arquivo.csv',
+                        sep=';', #Tipo de separador
+                        encoding = 'latin1')#caso contenha caracteres especiais
+
+#%%
